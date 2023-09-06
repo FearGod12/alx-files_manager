e237ec009e2c52f18ed2f4e27503f18ef0bc76e2
FearGod12 <onyenikechukwudi@gmail.com>
2023-09-06 23:37:39 +0100

    task 9

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const bodyParser = require('body-parser');
 const router = require('./routes/index');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.json());
+app.use(bodyParser.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(router);
 
